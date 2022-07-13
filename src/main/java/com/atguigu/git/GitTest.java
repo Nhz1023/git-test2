927877591f7e94abf8cca7af8ee5b6b757cd116b
Nhz <1067276853@qq.com>
2022-07-13 15:57:56 +0800

    my push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("master commit");
         System.out.println("hot-fix commit2");
+        System.out.println("push test");
     }
 }
